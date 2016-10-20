7dcd911c7647c87bb2bbe5cf37dd421266faa481
supriyamacwan <supriya.macwan10@gmail.com>
2016-10-20 13:47:27 +0530

    LiveFeedBackApp

---
+++ app/src/main/java/com/example/hammerhead/livefeedbackapp/FeedbackPageInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.hammerhead.livefeedbackapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedbackPageInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feedback_page_info_act);
+    }
+}
