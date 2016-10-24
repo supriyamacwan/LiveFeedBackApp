374d7baa0f466fdd7425e7735ae311194f2019af
supriyamacwan <supriya.macwan10@gmail.com>
2016-10-24 19:46:01 +0530

    Updated MainActivity.java

--- app/src/main/java/com/example/hammerhead/livefeedbackapp/FeedbackPageInfoActivity.java
+++ app/src/main/java/com/example/hammerhead/livefeedbackapp/FeedbackPageInfoActivity.java
@@ -1,13 +1,29 @@
 package com.example.hammerhead.livefeedbackapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class FeedbackPageInfoActivity extends AppCompatActivity {
+
+    Button givefeedbackbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feedback_page_info_act);
+        givefeedbackbutton = (Button)findViewById(R.id.feedback_button);
+        givefeedbackbutton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(FeedbackPageInfoActivity.this, GiveFeedbackPageActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
