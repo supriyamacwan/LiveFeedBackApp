3657e300bdee12e27ee96c5a43fdd64516d77985
supriyamacwan <supriya.macwan10@gmail.com>
2016-10-24 19:52:08 +0530

    Updatedfile

--- app/src/main/java/com/example/hammerhead/livefeedbackapp/GiveFeedbackPageActivity.java
+++ app/src/main/java/com/example/hammerhead/livefeedbackapp/GiveFeedbackPageActivity.java
@@ -3,11 +3,12 @@ package com.example.hammerhead.livefeedbackapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 public class GiveFeedbackPageActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.give_feedback_page_frag);
+        setContentView(R.layout.activity_give_feedback_page);
     }
+
 }
