4a857069c76c2830c1fe01ffd10775e9da10469b
supriyamacwan <supriya.macwan10@gmail.com>
2016-11-25 13:00:18 +0530

    coded for post button to enter data to firebase

--- app/src/main/java/com/example/hammerhead/livefeedbackapp/GiveFeedbackPageActivity.java
+++ app/src/main/java/com/example/hammerhead/livefeedbackapp/GiveFeedbackPageActivity.java
@@ -1,14 +1,42 @@
 package com.example.hammerhead.livefeedbackapp;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 
 public class GiveFeedbackPageActivity extends AppCompatActivity {
+    Button postbutton;
+    EditText username;
+    EditText feedbackquestion;
+    private DatabaseReference database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_give_feedback_page);
+        FirebaseHelper.init();
+        username = (EditText)findViewById(R.id.user_name);
+        feedbackquestion = (EditText)findViewById(R.id.feedback_question);
+        postbutton = (Button)findViewById(R.id.post_button);
+        postbutton.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               String name = username.getText().toString();
+                                               String question = feedbackquestion.getText().toString();
+                                           }
+                                       });
+
+
+
     }
 
 }
