999836765e5215ddc2c072e4c3c76f84a7305768
supriyamacwan <supriya.macwan10@gmail.com>
2016-11-25 12:57:49 +0530

    Created FirebaseHelper class for interaction between firbase and android studio

--- app/src/main/java/com/example/hammerhead/livefeedbackapp/FirebaseHelper.java
+++ app/src/main/java/com/example/hammerhead/livefeedbackapp/FirebaseHelper.java
@@ -1,8 +1,25 @@
 package com.example.hammerhead.livefeedbackapp;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by hammerhead on 11/23/2016.
  */
 
 public class FirebaseHelper {
+    private static FirebaseDatabase firebaseDatabase;
+    private static DatabaseReference usernameDatabaseReference;
+    public static void init() {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        usernameDatabaseReference = firebaseDatabase.getReference().child("username");
+
+
+    }
+
+
+
+
+
+
 }
